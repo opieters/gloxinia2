70b2762c333fd347857454e4eb6f4cf5c853be24
Olivier Pieters <olivier.pieters@ugent.be>
2023-08-02 12:15:33 +0200

    Remove old library code.

--- firmware/lib/gloxinia_common.c
+++
@@ -1,9 +0,0 @@
-#include <gloxinia_common.h>
-
-uint8_t gloxinia_i2c_get_address(gloxinia_config_t* config){
-    uint16_t address = SYLVATICA_I2C_BASE_ADDRESS;
-    address = address 
-            | (GET_BIT(config->address_selection1.port_r, config->address_selection1.n) << 1)
-            | GET_BIT(config->address_selection0.port_r, config->address_selection0.n);
-    return address;
-}
